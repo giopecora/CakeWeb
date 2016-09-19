d7fed6685edc5bb1c10d2703f1571858c67b12a9
Giovane Pecora <Giovane Pecora@192.168.0.32>
2016-09-19 15:09:17 -0300

    Comunicação Servlet e JS

--- src/main/java/servlets/TelaInicialProdutos.java
+++ src/main/java/servlets/TelaInicialProdutos.java
@@ -1,17 +1,17 @@
 package servlets;
+
 import classes.Produtos;
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 /**
  *
- * @author giovane.gpecora1
+ * @author Giovane Pecora
  */
-
-public class TelaInicial extends HttpServlet {
+public class TelaInicialProdutos extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -21,13 +21,18 @@ public class TelaInicial extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/JSP/TelaInicial.jsp");
-        rd.forward(request, response);
+        
+        Produtos produtos = new Produtos("CupCake", "Bolo pequeno projetado para uma pessoa", "./imagens/CupCake.jpg", 0);
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(produtos.getImagem());
+        
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        processRequest(request, response);
     }
 
     @Override
--- src/main/webapp/JS/CakeWeb.js
+++ src/main/webapp/JS/CakeWeb.js
@@ -1,26 +1,35 @@
 var imagens = ["./imagens/CupCake.jpg", "./imagens/Torta.jpg", "./imagens/beijinho.jpg", "./imagens/brigadeiro.jpg"];
-var titulos =["CupCake", "Torta", "Beijinho","Brigadeiro"];
-var descricoes = ["Bolo pequeno projetado para uma pessoa","Torta de Palmito, serve 10 pessoas","Brigadeiro com chocolate Belga","Beijinho com leite condensado importado"];
+var titulos = ["CupCake", "Torta", "Beijinho", "Brigadeiro"];
+var descricoes = ["Bolo pequeno projetado para uma pessoa", "Torta de Palmito, serve 10 pessoas", "Brigadeiro com chocolate Belga", "Beijinho com leite condensado importado"];
 var contImg = 0;
 
-$(document).ready(function (){
-    
-    $.get('TelaInicial', function(data) {
-        alert(data);
-    });
-    
-    $("#CupCake").attr("src",imagens[contImg]);
 
-    $("#setaDireita").click(function(){
-        if(contImg === 3){
+/*$(document).ready(function () {
+    var name = $('#userName').val();
+    $.get('GetUserServlet', function (responseText) {
+        $('#ajaxGetUserServletResponse').text(responseText);
+    });
+
+});*/
+
+$(document).ready(function () {
+    
+    $.get('TelaInicialProdutos', function (imagem) {
+        $("#CupCake").attr("src", imagem);
+    });
+
+    
+
+    $("#setaDireita").click(function () {
+        if (contImg === 3) {
             contImg = -1;
-	   }
-	   contImg++;
-	   trocarValoresProd();
+        }
+        contImg++;
+        trocarValoresProd();
     });
 
-    $("#setaEsquerda").click(function(){
-        if(contImg === 0){
+    $("#setaEsquerda").click(function () {
+        if (contImg === 0) {
             contImg = 4;
         }
         contImg--;
@@ -30,8 +39,8 @@ $(document).ready(function (){
 
 });
 
-function trocarValoresProd(){
-    $("#CupCake").attr("src",imagens[contImg]);
+function trocarValoresProd() {
+    $("#CupCake").attr("src", imagens[contImg]);
     $("#tituloImg").text(titulos[contImg]);
     $("#descricaoImg").text(descricoes[contImg]);
 }
